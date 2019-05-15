bfdfffd58eb8fe51ae3f7621eb1f435f1387f88e
Aynur J. Abdullayeva <AynurJa@CODE.LOCAL>
2019-05-15 17:23:11 +0400

    homeworkJS

---
+++ homework/js/main.js
@@ -0,0 +1,7 @@
+function openA(){
+    document.querySelector("#side").style.marginLeft="0px";
+    document.querySelector("#side").style.transition="1s"
+}
+function closeA(){
+    document.querySelector("#side").style.marginLeft="-200px";
+}
